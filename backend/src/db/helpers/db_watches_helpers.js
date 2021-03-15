9dd6e4c002a86b1ab0b953bb794a8e8ebcd56c2b
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-15 13:28:16 -0400

    create db helper functions for watch table

---
+++ backend/src/db/helpers/db_watches_helpers.js
@@ -0,0 +1,32 @@
+const { query } = require("../index");
+const db = require("../index");
+
+const addWatch = function (userID, ticker, value) {
+  const queryString = `
+  INSERT INTO user_watches
+  (user_id, ticker, value)
+  VALUES($1, $2, $3)
+  RETURNING *;`;
+
+  return db
+    .query(queryString, [userID, ticker, value])
+    .then((res) => res.rows[0])
+    .catch((err) => console.log("query error", err.stack));
+};
+
+const deleteWatch = function (watch_id) {
+  const queryString = `
+  UPDATE user_watches SET is_active = false 
+  WHERE id = $1
+  RETURNING *;`;
+
+  return db
+    .query(queryString, [watch_id])
+    .then((res) => res.rows[0])
+    .catch((err) => console.log("query error", err.stack));
+};
+
+module.exports = {
+  addWatch,
+  deleteWatch,
+};
