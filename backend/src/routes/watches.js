03f546c22e88b1ffb534b65cf42e54df283d8031
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-15 13:29:57 -0400

    router for watch requests

---
+++ backend/src/routes/watches.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.post("/", (req, res) => {
+    const { userId, ticker, value } = req.body;
+
+    db.addWatch(userId, ticker, value)
+      .then((resources) => {
+        res.json({ resources });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.put("/:watch_id", (req, res) => {
+    const { watch_id } = req.params;
+
+    db.deleteWatch(watch_id)
+      .then((resources) => {
+        res.json({ resources });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  return router;
+};
