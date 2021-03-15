6da9d28270e05486bd8074e38aba654701d1459c
Emily Lim <emily.elim04@gmail.com>
2021-03-15 16:29:00 +0000

    add db helper for user info update

--- backend/src/db/helpers/db_user_helpers.js
+++ backend/src/db/helpers/db_user_helpers.js
@@ -21,14 +21,38 @@ const getUserById = function(id) {
   WHERE id = $1`, 
   [id])
     .then(res => res.rows[0])
-    .catch(err => console.error('There has query error'));
+    .catch(err => console.error('There has query error', err.stack));
 
 };
 
 exports.getUserById = getUserById;
 
-const updateUserInfo = function(id, username, email) {
-
-
+const updateUserInfo = function(id, options) {
   
+  let queryParams = [];
+
+  let queryString = `UPDATE users `;
+
+  if (options.username) {
+    queryParams.push(`${options.username}`);
+    queryString += `SET username = $${queryParams.length} `
+  }
+
+  if (options.email) {
+    queryParams.push(`${options.email}`);
+    queryString += `SET email = $${queryParams.length} ` 
+  }
+
+  //add user id to queryParms list 
+  queryParams.push(`${id}`);
+
+  queryString += `
+  WHERE id = $${queryParams.length}
+  RETURNING username, email;
+  `
+  return db.query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(err => console.error('There has query error', err.stack))
 };
+
+exports.updateUserInfo = updateUserInfo;
