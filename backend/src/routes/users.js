5bab042343170c9ea308487f719920e9be962a47
Emily Lim <emily.elim04@gmail.com>
2021-03-15 15:00:18 -0400

    create user routes

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -6,16 +6,45 @@ module.exports = (db) => {
   //user's profile page with liked graphs and watched tickers and general news
   router.get('/:user_email', (req, res) => {
 
-    db.getUserByEmail(email)
-      .then()
-      .catch();
+    const { user_email } = req.body;
+
+    db.getUserByEmail(user_email)
+      .then((resources) => {
+        res.json({ resources })
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+
+  });
+
+  //add user to database 
+  router.post('/:user_email', (req, res) => {
+    const { user } = req.body; 
+
+    db.addUser(user)
+      .then((user) => {
+        res.json({ user })
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message })
+      });
+  });
+
+  //update user's info (email, phone number or username)
+  router.put('/:user_id', (req, res) => {
+    const { user_id, userChanges } = req.body;
+
+    db.updateUserInfo(user_id, userChanges)
+      .then((user) => {
+        res.json( { user })
+      })
+      .catch(res.status(500).json({ error: err.message }))
 
   });
 
 
-  router.post('/me', (req, res) => {
 
-      
 
-  });
+  return router;
 }
