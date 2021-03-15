531160ec418047981e290fb1d2faf8d7ca366d34
Emily Lim <emily.elim04@gmail.com>
2021-03-15 17:26:16 +0000

    create removeLike db helper function

--- backend/src/db/helpers/db_likes_helpers.js
+++ backend/src/db/helpers/db_likes_helpers.js
@@ -1,8 +1,8 @@
 const db = require('./index');
 
-const addLike = function(id, ticker) {
+const addLike = function(userId, ticker) {
 
-  let queryParams = [id, ticker];
+  let queryParams = [userId, ticker];
 
   let queryString = `
     INSERT INTO user_likes
@@ -18,3 +18,24 @@ const addLike = function(id, ticker) {
 };
 
 exports.addLike = addLike;
+
+//update the users_like table is_active row to false
+
+const removeLike = function(id, ticker){
+  let queryParams = [id, ticker];
+
+  let queryString = `
+    UPDATE user_likes
+    SET isActive = false
+    WHERE id = $1
+    RETURNING id;
+    `
+
+  return db.query(queryString, queryParams)
+    .then(res => res.rows[0])
+    .catch(err => console.error('There has query error', err.stack));
+
+};
+
+exports.removeLike = removeLike;
+
