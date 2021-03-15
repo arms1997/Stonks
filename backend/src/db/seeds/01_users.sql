b5b293367e831fc091cb46375e79fc777b2aca6e
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-15 11:09:30 -0400

    setup seed files and db reset

---
+++ backend/src/db/seeds/01_users.sql
@@ -0,0 +1,6 @@
+-- Users table seeds here (Example)
+INSERT INTO users (username, email, phone_number) VALUES ('alice_stonks', 'aliceinwonderland@gmail.com', '1234567890');
+INSERT INTO users (username, email, phone_number) VALUES ('kira_stonks', 'kira@gmail.com', '1234567890');
+INSERT INTO users (username, email, phone_number) VALUES ('emily_stonks', 'ilovebbyoda@mail.com', '1234567890');
+INSERT INTO users (username, email, phone_number) VALUES ('alvin_stonks', 'ngoalvin97@gmail.com', '1234567890');
+INSERT INTO users (username, email, phone_number) VALUES ('armeen_stonks', 'armeen.hadizadeh@gmail.com', '1234567890');
