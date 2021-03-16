b78bf4e1003d74e62148c94f4b1987ca7cca3696
Emily Lim <emily.elim04@gmail.com>
2021-03-16 13:41:01 -0400

    create login route

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Login from './Login';
+
 import "./App.css";
+import { Container } from '@material-ui/core';
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
-        <Container maxWidth="sm" >
-          <Signup />
+        <Container >
+          <Router>
+            <Switch>
+              <Route exact path='/'component={Dashboard}/>
+              <Route path='/me' component={Dashboard}/>
+              <Route path='/signup' component={Signup}/>
+              <Route path='/login' component={Login}/>
+            </Switch>
+          </Router>
         </Container>
       </div>
     </ AuthProvider>
