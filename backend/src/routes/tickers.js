ac953a8b5924d602b31253cda0f01680e73a1787
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-16 11:18:29 -0400

    cleanup and file relocation

--- backend/src/routes/tickers.js
+++ backend/src/routes/tickers.js
@@ -1,84 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
-const NewsApi = require("newsapi");
+const rp = require("request-promise");
 
-const dataParser = require("../dataHelpers/dataParser");
+const getNewsData = require("../dataHelpers/getNewsData");
+const getTickerData = require("../dataHelpers/getTickerData");
 
-require("dotenv").config();
-
-const newsapi = new NewsApi(process.env.NEWS_API);
+const { dataParser, domainParser } = require("../dataHelpers/dataParser");
 
 module.exports = () => {
   router.get("/", (req, res) => {
-    request(
-      `https://api.hypercharts.co/v1/companies?apiKey=${process.env.HYPER_API}`,
-      (error, response, body) => {
-        if (error) {
-          res.status(500).send({ error: error.message });
-          return;
-        }
-
-        res.send(body);
-      }
-    );
+    rp.get(
+      `https://api.hypercharts.co/v1/companies?apiKey=${process.env.HYPER_API}`
+    )
+      .then((response) => res.send(response))
+      .catch((err) => res.status(500).send({ err: err.message }));
   });
 
   router.get("/:symbol", (req, res) => {
     const { symbol } = req.params;
 
-    request(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&outputsize=full&apikey=${process.env.ALPHA_VANTAGE_API}`,
-      (error, response, body) => {
-        if (error) {
-          res.status(500).send({ error: error.message });
-          return;
-        }
-
-        const data = JSON.parse(body);
-
-        if (data["Note"]) {
-          console.log("rip");
-          res.status(500).send("Houston we got a problem");
-          return;
-        }
+    Promise.all([getTickerData(symbol), getNewsData(symbol)])
+      .then(([data, news]) => {
+        data = JSON.parse(data);
 
         const { parsedData, timestamps } = dataParser(
           data["Time Series (5min)"]
         );
 
-        const min = Math.floor(
-          Math.min.apply(
-            null,
-            parsedData.map((item) => item.y)
-          )
-        );
-        const max = Math.ceil(
-          Math.max.apply(
-            null,
-            parsedData.map((item) => item.y)
-          )
-        );
+        const { min, max } = domainParser(parsedData);
 
-        newsapi.v2
-          .everything({
-            sources: "the-wall-street-journal, bloomberg, business-insider",
-            qInTitle: `${symbol}`,
-            language: "en",
-            sortBy: "publishedAt",
-            from: "2021-02-15",
-          })
-          .then((response) => {
-            res.send({
-              yDomain: [min, max],
-              title: data["Meta Data"]["2. Symbol"],
-              data: parsedData,
-              timestamps,
-              news: response["articles"],
-            });
-          });
-      }
-    );
+        res.send({
+          yDomain: [min, max],
+          title: symbol,
+          data: parsedData,
+          timestamps,
+          news: news["articles"],
+        });
+      })
+      .catch((err) => console.error(err));
   });
 
   return router;
