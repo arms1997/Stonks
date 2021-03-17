982c2de6c5fb1d8e2cc068182f5a6f9070c0e5fd
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 06:34:01 -0400

    finishing news routes

--- backend/src/dataHelpers/getNewsData.js
+++ backend/src/dataHelpers/getNewsData.js
@@ -1,12 +1,27 @@
 const NewsApi = require("newsapi");
 
-const newsapi = new NewsApi(process.env.NEWS_API);
+const newsapi = new NewsApi("e622835931cb4955b3b3601c49020292");
 
-module.exports = (symbol) => {
+const getAllNews = () => {
   return newsapi.v2.everything({
     sources: "the-wall-street-journal, bloomberg, business-insider",
-    qInTitle: `${symbol}`,
     language: "en",
     sortBy: "publishedAt",
+    pageSize: 100,
   });
+};
+
+const getCompanyNews = (company, symbol) => {
+  return newsapi.v2.everything({
+    sources: "the-wall-street-journal, bloomberg, business-insider",
+    qInTitle: `${company} OR ${symbol}`,
+    language: "en",
+    sortBy: "publishedAt",
+    pageSize: 100,
+  });
+};
+
+module.exports = {
+  getCompanyNews,
+  getAllNews,
 };
--- backend/src/routes/tickers.js
+++ backend/src/routes/tickers.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const rp = require("request-promise");
+const moment = require("moment");
 
 const getNewsData = require("../dataHelpers/getNewsData");
 const getTickerData = require("../dataHelpers/getTickerData");
 
-const { dataParser, domainParser } = require("../dataHelpers/dataParser");
+const {
+  dataParser,
+  domainParser,
+  getChangeInData,
+  getRelevantNews,
+  getAreaData,
+  getHintData,
+} = require("../dataHelpers/dataParser");
 
 module.exports = () => {
   router.get("/", (req, res) => {
@@ -16,16 +24,28 @@ module.exports = () => {
       .catch((err) => res.status(500).send({ err: err.message }));
   });
 
-  router.get("/:symbol", (req, res) => {
-    const { symbol } = req.params;
+  router.get("/query", (req, res) => {
+    const { symbol, company } = req.query;
 
-    Promise.all([getTickerData(symbol), getNewsData(symbol)])
+    console.log(symbol, company);
+
+    Promise.all([getTickerData(symbol), getNewsData(company)])
       .then(([data, news]) => {
         data = JSON.parse(data);
 
         const { parsedData, timestamps } = dataParser(
           data["Time Series (5min)"]
         );
+
+        const deltaData = getChangeInData(data["Time Series (5min)"]);
+
+        news = news["articles"].filter((article) => article.content);
+
+        const relevantNews = getRelevantNews(news, deltaData);
+
+        const areaData = getAreaData(parsedData, timestamps, relevantNews);
+
+        const hintData = getHintData(areaData, relevantNews);
 
         const { min, max } = domainParser(parsedData);
 
@@ -34,7 +54,9 @@ module.exports = () => {
           title: symbol,
           data: parsedData,
           timestamps,
-          news: news["articles"],
+          areaData,
+          hintData,
+          relevantNews,
         });
       })
       .catch((err) => console.error(err));
