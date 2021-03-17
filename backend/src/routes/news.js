141502ef7bc5ed7282b0cb5065d901f398e83e88
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 15:07:52 -0400

    change return news object

--- backend/src/routes/news.js
+++ backend/src/routes/news.js
@@ -7,7 +7,23 @@ module.exports = () => {
   //Get All news
   router.get("/", (req, res) => {
     getAllNews()
-      .then((news) => res.send(news))
+      .then((news) => {
+        news = news["articles"].filter((article) => article.content);
+
+        news = news.map((article) => {
+          return {
+            title: article.title,
+            description: article.description,
+            image: article.urlToImage,
+            author: article.author,
+            source: article.source.name,
+            url: article.url,
+            publishedAt: article.publishedAt,
+          };
+        });
+
+        res.send(news);
+      })
       .catch((err) => res.status(500).send({ err: err.message }));
   });
 
@@ -15,7 +31,23 @@ module.exports = () => {
     const { company, symbol } = req.query;
 
     getCompanyNews(company, symbol)
-      .then((news) => res.send(news))
+      .then((news) => {
+        news = news["articles"].filter((article) => article.content);
+
+        news = news.map((article) => {
+          return {
+            title: article.title,
+            description: article.description,
+            image: article.urlToImage,
+            author: article.author,
+            source: article.source.name,
+            url: article.url,
+            publishedAt: article.publishedAt,
+          };
+        });
+
+        res.send(news);
+      })
       .catch((err) => res.status(500).send({ err: err.message }));
   });
 
