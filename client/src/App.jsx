da2e8a44338df4f75fc5fc26516b63746bdad4df
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 15:06:37 -0400

    no changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      {/* <Graph symbol={"tsla"} company={"tesla"} /> */}
+      {/* <Graph symbol={"tsla"} company={"tesla"} showNews={true} /> */}
       <Navbar />
     </div>
   );
