21c2ede981b8eca3a57e0c5dda3c5bc8b15a4439
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 08:34:18 -0400

    making graph, add data hint elements

--- client/src/components/Graph/LineGraph.jsx
+++ client/src/components/Graph/LineGraph.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import {
   FlexibleWidthXYPlot,
   AreaSeries,
@@ -12,13 +11,20 @@ import {
   MarkSeries,
 } from "react-vis";
 import "../../node_modules/react-vis/dist/style.css";
+import moment from "moment";
 
 export default function LineGraph(props) {
   const [hoverdNode, setHoveredNode] = useState(null);
 
-  const _onMouseLeave = () => {
-    setHoveredNode(null);
+  const { timestamps, yDomain, data } = props;
+
+  const _onMouseLeave = () => setHoveredNode(null);
+
+  const _tickFormatter = (index) => {
+    return moment(props.timestamps[index]).format("MM-DD");
   };
+
+  const _onNearestX = (value) => setHoveredNode({ ...value });
 
   return (
     <div>
@@ -26,7 +32,21 @@ export default function LineGraph(props) {
         height={500}
         onMouseLeave={_onMouseLeave}
         yDomain={props.yDomain}
-      ></FlexibleWidthXYPlot>
+      >
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis tickFormat={_tickFormatter} />
+        <YAxis />
+        <LineSeries data={data} onNearestX={_onNearestX} animation />
+        {hoverdNode && <MarkSeries data={hoverdNode} />}
+        {hoverdNode && (
+          <Hint
+            value={hoverdNode}
+            align={{ horizontal: "leftEdge", vertical: "topEdge" }}
+            style={{ marginLeft: "80px", marginTop: "30px" }}
+          ></Hint>
+        )}
+      </FlexibleWidthXYPlot>
     </div>
   );
 }
