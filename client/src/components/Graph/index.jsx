1d1265fc48ac140ba585e366fc7ef137638737d6
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 09:09:59 -0400

    main graph file to manage which graph to load

---
+++ client/src/components/Graph/index.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LineGraph from "./LineGraph";
+
+export default function Graph(props) {
+  const [graphData, setGraphData] = useState(null);
+
+  useEffect(() => {
+    const request = props.company
+      ? axios.get(
+          `/api/tickers/graph?company=${props.company}&symbol=${props.symbol}`
+        )
+      : axios.get(`/api/tickers/${props.symbol}`);
+
+    request
+      .then((data) => setGraphData(data.data))
+      .catch((err) => console.error(err));
+  }, [props.symbol]);
+
+  return <div>{graphData && <LineGraph data={graphData} />}</div>;
+}
