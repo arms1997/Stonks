8df4ede2b4d3e9b5ba7ad97123785a82f2294ca5
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 11:29:19 -0400

    testing navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Graph from "./components/Graph";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Graph symbol={"tsla"} company={"tesla"} />
+      {/* <Graph symbol={"tsla"} company={"tesla"} /> */}
+      <Navbar />
     </div>
   );
 }
