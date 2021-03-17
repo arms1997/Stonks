edb9ddd0757e52ef674b1b6a6ba177aa929ea597
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 06:46:22 -0400

    finish get relevant news

--- backend/src/routes/news.js
+++ backend/src/routes/news.js
@@ -4,11 +4,18 @@ const router = express.Router();
 const { getAllNews, getCompanyNews } = require("../dataHelpers/getNewsData");
 
 module.exports = () => {
+  //Get All news
   router.get("/", (req, res) => {
     getAllNews()
-      .then((news) => {
-        res.send(news);
-      })
+      .then((news) => res.send(news))
+      .catch((err) => res.status(500).send({ err: err.message }));
+  });
+
+  router.get("/company", (req, res) => {
+    const { company, symbol } = req.query;
+
+    getCompanyNews(company, symbol)
+      .then((news) => res.send(news))
       .catch((err) => res.status(500).send({ err: err.message }));
   });
 
