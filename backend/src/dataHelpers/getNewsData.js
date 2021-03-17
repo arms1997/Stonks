5a528ac4abe5484f18d1c6f2208e5c8914c19e27
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 06:45:56 -0400

    add comments for functions

--- backend/src/dataHelpers/getNewsData.js
+++ backend/src/dataHelpers/getNewsData.js
@@ -2,6 +2,10 @@ const NewsApi = require("newsapi");
 
 const newsapi = new NewsApi("e622835931cb4955b3b3601c49020292");
 
+/**
+ * Returns all news from sources specified
+ * @return {Promise} promise that can be accessed to get news Data
+ */
 const getAllNews = () => {
   return newsapi.v2.everything({
     sources: "the-wall-street-journal, bloomberg, business-insider",
@@ -11,6 +15,12 @@ const getAllNews = () => {
   });
 };
 
+/**
+ * Returns specific news related to a company name and symbol from sources specified
+ * @param {string} company The name of the company
+ * @param {string} symbol The financial symbol of the compnay
+ * @return {Promise} promise that can be accessed to get news Data
+ */
 const getCompanyNews = (company, symbol) => {
   return newsapi.v2.everything({
     sources: "the-wall-street-journal, bloomberg, business-insider",
