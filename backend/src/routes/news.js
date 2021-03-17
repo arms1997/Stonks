85865c203320e65995873b8add49966f360f2a0d
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 06:39:38 -0400

    finish getallnews

--- backend/src/routes/news.js
+++ backend/src/routes/news.js
@@ -5,8 +5,12 @@ const { getAllNews, getCompanyNews } = require("../dataHelpers/getNewsData");
 
 module.exports = () => {
   router.get("/", (req, res) => {
-    getAllNews.then((news) => {
-      console.log(news);
-    });
+    getAllNews()
+      .then((news) => {
+        res.send(news);
+      })
+      .catch((err) => res.status(500).send({ err: err.message }));
   });
+
+  return router;
 };
