b744f24c6868bfdff7b022c05bd33661bfeb073e
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-18 15:07:30 -0400

    styling changes

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
-import { getUserBackend } from './Auth_Helpers';
+import { getUserBackend } from "./Auth_Helpers";
 
-//Create context for all of app to use 
+//Create context for all of app to use
 const AuthContext = React.createContext();
 
 //Function for using context across app
@@ -13,72 +13,64 @@ export function useAuth() {
 
 //Use auth context in provider and have it render it in all of its children
 export function AuthProvider({ children }) {
-
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-  };
-
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
 
   function login(email, password) {
     return auth.signInWithEmailAndPassword(email, password);
-  };
+  }
 
   function logout() {
-    return auth.signOut()
-  };
+    return auth.signOut();
+  }
 
   function resetPassword(email) {
     return auth.sendPasswordResetEmail(email);
-  };
-
+  }
 
   function updateEmail(email) {
     return currentUser.updateEmail(email);
-  };
+  }
 
   function updatePassword(password) {
     return currentUser.updatePassword(password);
-  };
+  }
 
   useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      //make request to backend to add user likes/watches to currentUser object
+      console.log("currentuser", currentUser);
 
-    const unsubscribe = auth.onAuthStateChanged(user => {
-    
-    //make request to backend to add user likes/watches to currentUser object
-
-    if (user) {
-      getUserBackend(user.email)
-       .then((backendUserData) => {
-         user["likes"] = backendUserData.data.likes;
-         user["watches"] = backendUserData.data.watches;
-         user["username"] = backendUserData.data.username;
-         user["user_id"] = backendUserData.data.user_id;
-         user["user_phone_num"] = backendUserData.data.user_phone_num;
-       })
-       .then(() => {
-         setCurrentUser(user);
-         setLoading(false);
-        })
-       .catch(err => console.log(err))
-
-    } else {
-      setCurrentUser(user);
-      setLoading(false);
-    }
+      if (user) {
+        getUserBackend(user.email)
+          .then((backendUserData) => {
+            user["likes"] = backendUserData.data.likes;
+            user["watches"] = backendUserData.data.watches;
+            user["username"] = backendUserData.data.username;
+            user["user_id"] = backendUserData.data.user_id;
+            user["user_phone_num"] = backendUserData.data.user_phone_num;
+          })
+          .then(() => {
+            setCurrentUser(user);
+            setLoading(false);
+          })
+          .catch((err) => console.error("hello"));
+      } else {
+        setCurrentUser(user);
+        setLoading(false);
+      }
       // setLoading(false);
     });
 
     return unsubscribe;
-
-  }, [])
- 
+  }, []);
 
   //provide all the info we want to provide with auth
   const value = {
-    
     currentUser,
     signup,
     login,
@@ -86,14 +78,12 @@ export function AuthProvider({ children }) {
     resetPassword,
     updateEmail,
     updatePassword,
-    setCurrentUser
-  
-  }
+    setCurrentUser,
+  };
 
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
-  )
-
+  );
 }
