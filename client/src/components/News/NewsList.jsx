9cc033bc06e672137270988e6452e863dcd62baf
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-18 16:38:58 -0400

    change console.error on line 16

--- client/src/components/News/NewsList.jsx
+++ client/src/components/News/NewsList.jsx
@@ -1,43 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import NewsListItem from "./NewsListItem";
 
-import NewsListItem from './NewsListItem';
-
-export default function NewsList({company, symbol}) {
-
+export default function NewsList({ company, symbol }) {
   const [newsData, setNewsData] = useState(null);
 
   function getNews(company, symbol) {
-    return axios.get(`/api/news/company?company=${company}&symbol=${symbol}`)
+    return axios.get(`/api/news/company?company=${company}&symbol=${symbol}`);
   }
 
   useEffect(() => {
     getNews(company, symbol)
       .then((newsData) => setNewsData(newsData.data))
-      .catch((err) => console.err(err))
-  }, [company, symbol])
+      .catch((err) => console.error(err));
+  }, [company, symbol]);
 
-  const parsedArticles = newsData && newsData.map((article, index) => {
+  const parsedArticles =
+    newsData &&
+    newsData.map((article, index) => {
+      return (
+        <NewsListItem
+          key={index}
+          title={article.title}
+          description={article.description}
+          image={article.image}
+          author={article.author}
+          source={article.source}
+          url={article.url}
+          publishedDate={article.publishedAt}
+        />
+      );
+    });
 
-    return (
-      <NewsListItem
-        key={index}
-        title={article.title}
-        description={article.description}
-        image={article.image}
-        author={article.author}
-        source={article.source}
-        url={article.url}
-        publishedDate={article.publishedAt}
-      />
-    )
-
-  })
-
-  return (
-    <>
-      {parsedArticles}
-    </>
-  );
+  return <>{parsedArticles}</>;
 }
