3a8c47ad294f66ae1e6ecbd1d6480a898ac0570e
Emily Lim <emily.elim04@gmail.com>
2021-03-18 12:55:29 -0400

    start to style profile update page

--- client/src/components/ProfileSettings/UpdateProfile.jsx
+++ client/src/components/ProfileSettings/UpdateProfile.jsx
@@ -1,13 +1,42 @@
 
+import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+
+import { Avatar, makeStyles } from "@material-ui/core";
 
 import Form from './Form';
 
+import './ProfileSettings.scss';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#3f51b5",
+  },
+  
+}));
 
 export default function UpdateProfile() {
 
-  
-  return (
+  const classes = useStyles();
 
-    <Form />
+  const { currentUser } = useAuth();
+
+
+  return (
+    <main className="profile">
+      <header className="profile__header">
+        <Avatar src="/broken-image.jpg" className={classes.large}/>
+        <h1 className="profile__header-username">{currentUser.username}</h1>
+      </header>
+      <h2 className="profile__title">Account Settings</h2>
+      <Form />
+      <footer>
+            <Link to="/">Go Back To Home Page</Link>
+      </footer>
+    </main>
   )
 }
