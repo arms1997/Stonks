72be7b45bec1ce8b84f660d3cf9fcbf32a886f50
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-18 17:26:32 -0400

    working on detail component for company info

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -1,6 +1,15 @@
-export default Detail(){
-  return (
-    <div>
-    </div>
-  )
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Detail({ symbol }) {
+  const [companyData, setCompanyData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`/api/tickers/company/${symbol}`)
+      .then((data) => setCompanyData(data.data))
+      .catch((error) => console.error(error));
+  }, [symbol]);
+
+  return <div>{companyData.name}</div>;
 }
