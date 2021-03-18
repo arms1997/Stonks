ebc246dea5aeb8c4fab4164a5faa05103be62c79
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-17 21:26:19 -0400

    working on navbar

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 
-import { Button, Card, Container, TextField } from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import { useAuth } from '../contexts/AuthContext';
-import { Link, Redirect, useHistory } from 'react-router-dom';
-
+import { Button, Card, Container, TextField } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, Redirect, useHistory } from "react-router-dom";
 
 export default function Login() {
-
   const emailRef = useRef();
   const passwordRef = useRef();
 
@@ -16,12 +14,11 @@ export default function Login() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    try {;
-      setError("")
+    try {
+      setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
       history.push("/me");
@@ -34,29 +31,53 @@ export default function Login() {
 
   return (
     <>
-    {(currentUser) ? <Redirect to="/"/> : 
-      <Container maxWidth="sm">
-        <Card>
-          <h2>Log In</h2>
-          {error && <Alert severity="error">{error}</Alert>}
-          <form onSubmit={handleSubmit}>
-            {/* <label for="email">Email</label> */}
-            <TextField required className="standard-required" label="Required" helperText="Email" type="email" inputRef={emailRef} />
-            {/* <input name="email" type="email" ref={emailRef} /> */}
-            {/* <label for="password">Password</label> */}
-            <TextField required className="standard-required" label="Required" helperText="Password" type="password" inputRef={passwordRef} />
-            {/* <input name="password" type="password" ref={passwordRef} /> */}
-            <Button disabled={loading} variant="contained" color="primary" type="submit">Log in!</Button>
-          </form>
-          <div> 
-            <Link to="/forgot-password">Forgot password?</Link>
+      {currentUser ? (
+        <Redirect to="/" />
+      ) : (
+        <Container maxWidth="sm">
+          <Card>
+            <h2>Log In</h2>
+            {error && <Alert severity="error">{error}</Alert>}
+            <form onSubmit={handleSubmit}>
+              {/* <label for="email">Email</label> */}
+              <TextField
+                required
+                className="standard-required"
+                label="Required"
+                helperText="Email"
+                type="email"
+                inputRef={emailRef}
+              />
+              {/* <input name="email" type="email" ref={emailRef} /> */}
+              {/* <label for="password">Password</label> */}
+              <TextField
+                required
+                className="standard-required"
+                label="Required"
+                helperText="Password"
+                type="password"
+                inputRef={passwordRef}
+              />
+              {/* <input name="password" type="password" ref={passwordRef} /> */}
+              <Button
+                disabled={loading}
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Log in!
+              </Button>
+            </form>
+            <div>
+              <Link to="/forgot-password">Forgot password?</Link>
+            </div>
+          </Card>
+          <div>
+            Need an account?
+            <Link to="/signup">Sign up!</Link>
           </div>
-        </Card> 
-        <div> 
-          Need an account?
-          <Link to="/signup">Sign up!</Link>
-        </div>
-      </Container> }
+        </Container>
+      )}
     </>
-  )
+  );
 }
