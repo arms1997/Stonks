2a5037d551b5c9da7988d5edd2a61cb713e08d39
Emily Lim <emily.elim04@gmail.com>
2021-03-17 21:18:31 -0400

    remove unused styling

--- client/src/components/News/NewsListItem.jsx
+++ client/src/components/News/NewsListItem.jsx
@@ -1,14 +1,7 @@
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
+import { Card, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
