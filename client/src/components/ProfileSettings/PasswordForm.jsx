cfe85360ca3123eacaa1bafedb2e5d9d28f66723
Emily Lim <emily.elim04@gmail.com>
2021-03-18 18:27:19 -0400

    refactor update profile functions and separate password form

---
+++ client/src/components/ProfileSettings/PasswordForm.jsx
@@ -0,0 +1,97 @@
+import React, { useRef, useState } from "react";
+
+import { Button, TextField } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import { useAuth } from "../../contexts/AuthContext";
+
+import "./ProfileSettings.scss";
+
+import "./ProfileSettings.scss";
+
+export default function PasswordForm() {
+
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  const {
+    currentUser,
+    updateUser
+  } = useAuth();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  function handlePasswordSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Oh no, passwords do not match!");
+    }
+
+    //object to send to backend
+    const userChanges = {};
+
+    //conditionally add to object current users changes to be implemented
+    
+    if (passwordRef.current.value) {
+      userChanges["password"] = passwordRef.current.value;
+    }
+
+    setLoading(true);
+    setError("");
+    setMessage("");
+
+    updateUser(userChanges)
+      .then(() => {
+        setMessage("Your password has been updated.");
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setError("Failed to update account.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <div className="profile__alert">
+        {message && <Alert severity="success">{message}</Alert>}
+        {error && <Alert severity="error">{error}</Alert>}
+      </div>
+      <div className="profile__box">
+        <form onSubmit={handlePasswordSubmit} className="profile__box-form">
+          <TextField
+            className="profile__box-form-textfield"
+            id="outlined-basic"
+            variant="outlined"
+            label="Password"
+            type="password"
+            placeholder="Leave blank if no change"
+            inputRef={passwordRef}
+          />
+          <TextField
+            className="profile__box-form-textfield"
+            id="outlined-basic"
+            variant="outlined"
+            label="Password Confirmation"
+            type="password"
+            placeholder="Leave blank if no change"
+            inputRef={passwordConfirmRef}
+          />
+          <Button
+            className="profile__box-form-button"
+            disabled={loading}
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Update
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+}
