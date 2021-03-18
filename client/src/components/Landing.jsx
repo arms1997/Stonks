99e620fc7c59c78da3dd00a8f2682c3534fd130d
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-18 08:33:06 -0400

    no change to code just styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,9 +1,8 @@
-
-import React, { useState } from "react"
-import { Card, Button } from "@material-ui/core"
+import React, { useState } from "react";
+import { Card, Button } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
-import { useAuth } from "../contexts/AuthContext"
-import { useHistory } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext";
+import { useHistory } from "react-router-dom";
 
 export default function Landing() {
   const [error, setError] = useState("");
@@ -12,27 +11,25 @@ export default function Landing() {
   const history = useHistory();
 
   async function handleLogout() {
-    setError("")
+    setError("");
     try {
-      await logout()
-      history.push("/login")
+      await logout();
+      history.push("/login");
     } catch {
-      setError("Failed to log out")
+      setError("Failed to log out");
     }
   }
 
   return (
     <>
       <Card>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert severity="error">{error}</Alert>}
-          <strong>Email:</strong> {currentUser && currentUser.email}
+        <h2 className="text-center mb-4">Profile</h2>
+        {error && <Alert severity="error">{error}</Alert>}
+        <strong>Email:</strong> {currentUser && currentUser.email}
       </Card>
       <div className="w-100 text-center mt-2">
-      <Button onClick={handleLogout}>
-        Log Out
-      </Button>
+        <Button onClick={handleLogout}>Log Out</Button>
       </div>
     </>
-  )
+  );
 }
