1a7a479571f53c57f0e1edca04903015070101c3
Emily Lim <emily.elim04@gmail.com>
2021-03-19 13:07:31 -0400

    create basic watch table

--- client/src/components/UserLanding/UserLanding.jsx
+++ client/src/components/UserLanding/UserLanding.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import LineGraphNews from "../Graph/LineGraphNews";
+
+import NewsList from "../News/NewsList";
+import WatchTable from "./WatchTable";
+
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function UserLanding() {
-  return <div>User's page</div>;
+  const { currentUser } = useAuth();
+  return (
+    <div>
+      <WatchTable userWatches={currentUser.watches}></WatchTable>
+      User's page
+    </div>
+  );
 }
