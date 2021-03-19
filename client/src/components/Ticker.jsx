fb51709a7b1c78a0e562442a5b632d5092920436
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-19 13:37:05 -0400

    working on card element for graph on ticker page

--- client/src/components/Ticker.jsx
+++ client/src/components/Ticker.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLoading, Audio } from "@agney/react-loading";
 
 import Detail from "./Detail";
@@ -6,25 +6,59 @@ import Graph from "./Graph";
 import NewsList from "./News/NewsList";
 
 import "./Ticker.scss";
+import { Card, CardActions, IconButton, makeStyles } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import { blue } from "@material-ui/core/colors";
+import { useAuth } from "../contexts/AuthContext";
+
+const useStyles = makeStyles({
+  cardBottom: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+});
 
 export default function Ticker({ symbol, company }) {
   const [loading, setLoading] = useState(true);
+  const [liked, setLiked] = useState(false);
+  const classes = useStyles();
+
+  const { currentUser } = useAuth();
 
   const { containerProps, indicatorEl } = useLoading({
     loading: loading,
     indicator: <Audio width="100" />,
   });
 
+  useEffect(() => {
+    if (currentUser.likes.includes(symbol)) {
+      setLiked(true);
+    }
+  }, [currentUser, symbol]);
+
   return (
     <div className="ticker">
-      <section {...containerProps}>{indicatorEl}</section>
-      <Graph
-        symbol={symbol}
-        company={company}
-        showNews={true}
-        loading={loading}
-        setLoading={setLoading}
-      />
+      <Card>
+        <section {...containerProps}>{indicatorEl}</section>
+        <Graph
+          symbol={symbol}
+          company={company}
+          showNews={true}
+          loading={loading}
+          setLoading={setLoading}
+        />
+        <CardActions className={classes.cardBottom}>
+          <div>
+            <IconButton onClick={() => setLiked(!liked)}>
+              <FavoriteIcon color={liked ? "primary" : "inherit"} />
+            </IconButton>
+            <IconButton>
+              <VisibilityIcon />
+            </IconButton>
+          </div>
+        </CardActions>
+      </Card>
       {/* {!loading && ( */}
       <div className="ticker__bottom">
         <NewsList symbol={symbol} company={company} />
