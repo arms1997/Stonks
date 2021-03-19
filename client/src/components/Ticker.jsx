25a00a2a3a09ffea9ecb2c717934763406007d40
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-19 09:54:44 -0400

    add loading feature to the page

--- client/src/components/Ticker.jsx
+++ client/src/components/Ticker.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useLoading, Audio } from "@agney/react-loading";
+
 import Detail from "./Detail";
 import Graph from "./Graph";
 import NewsList from "./News/NewsList";
@@ -7,20 +10,29 @@ import "./Ticker.scss";
 export default function Ticker({ symbol, company }) {
   const [loading, setLoading] = useState(true);
 
+  const { containerProps, indicatorEl } = useLoading({
+    loading: loading,
+    indicator: <Audio width="100" />,
+  });
+
   return (
     <div className="ticker">
+      <section {...containerProps}>{indicatorEl}</section>
       <Graph
         symbol={symbol}
         company={company}
         showNews={true}
+        loading={loading}
         setLoading={setLoading}
       />
-      <div className="ticker__bottom">
-        <NewsList symbol={symbol} company={company} />
-        <div className="ticker__bottom-detail">
-          <Detail symbol={symbol} />
+      {!loading && (
+        <div className="ticker__bottom">
+          <NewsList symbol={symbol} company={company} />
+          <div className="ticker__bottom-detail">
+            <Detail symbol={symbol} />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
