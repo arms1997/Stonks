614a197bc37a0cf7f2ebc176f01539bdb6230739
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-19 09:53:57 -0400

    add loading feature to graph

--- client/src/components/Graph/index.jsx
+++ client/src/components/Graph/index.jsx
@@ -2,23 +2,35 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import LineGraphNews from "./LineGraphNews";
 
-export default function Graph({ company, symbol, showNews = false }) {
+export default function Graph({
+  company,
+  symbol,
+  showNews = false,
+  loading,
+  setLoading,
+}) {
   const [graphData, setGraphData] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     const requestString = showNews
       ? `/api/tickers/graph?company=${company}&symbol=${symbol}`
       : `/api/tickers/${symbol}`;
 
     axios
       .get(requestString)
-      .then((data) => setGraphData(data.data))
+      .then((data) => {
+        setGraphData(data.data);
+        setLoading(false);
+      })
       .catch((err) => console.error(err));
-  }, [company, symbol, showNews]);
+  }, [company, symbol, showNews, setLoading]);
 
   return (
     <div>
-      {graphData && <LineGraphNews graphData={graphData} showNews={showNews} />}
+      {!loading && graphData && (
+        <LineGraphNews graphData={graphData} showNews={showNews} />
+      )}
     </div>
   );
 }
