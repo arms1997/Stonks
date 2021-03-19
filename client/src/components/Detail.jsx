1110fc10361b7539e7aba06792e30e435989c2ce
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-19 09:53:27 -0400

    cleanup

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -3,11 +3,9 @@ import { useEffect, useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { Collapse, FormHelperText, IconButton } from "@material-ui/core";
+import { Collapse, IconButton } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles({
