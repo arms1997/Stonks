46d6e3cad1ca91b6d018e0bf7229457d31ffb684
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-19 16:16:33 -0400

    create axios calls for likes functionality

--- client/src/contexts/Auth_Helpers.js
+++ client/src/contexts/Auth_Helpers.js
@@ -1,18 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 export function getUserBackend(userEmail) {
-  return axios.get(`/api/user/${userEmail}`)
-};
+  return axios.get(`/api/user/${userEmail}`);
+}
 
 export function addUserBackend(newUser) {
   return axios.post(`/api/user/:user_info`, {
-    user_info: newUser
-  })
-};
+    user_info: newUser,
+  });
+}
 
 export function updateUserBackend(user_id, userChanges) {
   return axios.put(`/api/user/:user_id`, {
     user_id: user_id,
-    user_info: userChanges
-  })
-};
+    user_info: userChanges,
+  });
+}
+
+export function likeTicker(user_id, ticker) {
+  return axios.post(`/api/like`, {
+    userId: user_id,
+    ticker: ticker,
+  });
+}
+
+export function updateLikeTicker(like_id) {
+  return axios.put(`/api/like/${like_id}`);
+}
