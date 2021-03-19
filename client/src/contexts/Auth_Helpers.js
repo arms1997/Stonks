1d3622a10364ac85fb18093dba9b19b05c48ac5d
Emily Lim <emily.elim04@gmail.com>
2021-03-19 15:43:13 -0400

    add removeWatch helper function

--- client/src/contexts/Auth_Helpers.js
+++ client/src/contexts/Auth_Helpers.js
@@ -1,18 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 export function getUserBackend(userEmail) {
-  return axios.get(`/api/user/${userEmail}`)
-};
+  return axios.get(`/api/user/${userEmail}`);
+}
 
 export function addUserBackend(newUser) {
   return axios.post(`/api/user/:user_info`, {
-    user_info: newUser
-  })
-};
+    user_info: newUser,
+  });
+}
 
 export function updateUserBackend(user_id, userChanges) {
   return axios.put(`/api/user/:user_id`, {
     user_id: user_id,
-    user_info: userChanges
-  })
-};
+    user_info: userChanges,
+  });
+}
+
+export function removeWatch(watchId) {
+  return axios.put(`/api/watch/${watchId}`);
+}
