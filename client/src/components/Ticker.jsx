9ef324d7c6b10dc8ff93c16f9573c4aa5f760b26
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-18 21:21:28 -0400

    ticker component almost done

--- client/src/components/Ticker.jsx
+++ client/src/components/Ticker.jsx
@@ -2,12 +2,25 @@ import Detail from "./Detail";
 import Graph from "./Graph";
 import NewsList from "./News/NewsList";
 
+import "./Ticker.scss";
+
 export default function Ticker({ symbol, company }) {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div>
-      <Graph symbol={symbol} company={company} showNews={true} />
-      <NewsList symbol={symbol} company={company} />
-      <Detail symbol={symbol} />
+    <div className="ticker">
+      <Graph
+        symbol={symbol}
+        company={company}
+        showNews={true}
+        setLoading={setLoading}
+      />
+      <div className="ticker__bottom">
+        <NewsList symbol={symbol} company={company} />
+        <div className="ticker__bottom-detail">
+          <Detail symbol={symbol} />
+        </div>
+      </div>
     </div>
   );
 }
