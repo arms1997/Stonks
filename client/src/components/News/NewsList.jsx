4a7634faad3bfaa376f3d4f08224e83e971fb435
Emily Lim <emily.elim04@gmail.com>
2021-03-18 22:16:05 -0400

    add show more button

--- client/src/components/News/NewsList.jsx
+++ client/src/components/News/NewsList.jsx
@@ -1,43 +1,62 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Collapse } from "@material-ui/core";
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import NewsListItem from "./NewsListItem";
+import "./NewsList.scss";
 
-import NewsListItem from './NewsListItem';
-
-export default function NewsList({company, symbol}) {
+export default function NewsList({ company, symbol }) {
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   const [newsData, setNewsData] = useState(null);
 
   function getNews(company, symbol) {
-    return axios.get(`/api/news/company?company=${company}&symbol=${symbol}`)
+    return axios.get(`/api/news/company?company=${company}&symbol=${symbol}`);
   }
 
   useEffect(() => {
     getNews(company, symbol)
       .then((newsData) => setNewsData(newsData.data))
-      .catch((err) => console.err(err))
-  }, [company, symbol])
+      .catch((err) => console.err(err));
+  }, [company, symbol]);
 
-  const parsedArticles = newsData && newsData.map((article, index) => {
+  const parsedArticles =
+    newsData &&
+    newsData.map((article, index) => {
+      return (
+        <NewsListItem
+          key={index}
+          title={article.title}
+          description={article.description}
+          image={article.image}
+          author={article.author}
+          source={article.source}
+          url={article.url}
+          publishedDate={article.publishedAt}
+        />
+      );
+    });
 
-    return (
-      <NewsListItem
-        key={index}
-        title={article.title}
-        description={article.description}
-        image={article.image}
-        author={article.author}
-        source={article.source}
-        url={article.url}
-        publishedDate={article.publishedAt}
-      />
-    )
+  // const showArticles = parsedArticles.slice(0, 6);
 
-  })
+  // const restOfArticles = parsedArticles.slice(5);
 
   return (
-    <>
-      {parsedArticles}
-    </>
+    <div>
+      {/* {parsedArticles.length < 1 && <h2>No Articles Available</h2>} */}
+      <h1>News</h1>
+      {/* {showArticles} */}
+      <Button
+        className="newsList__button"
+        variant="outlined"
+        onClick={handleExpandClick}
+      >
+        Show More
+      </Button>
+      <Collapse in={expanded}>{parsedArticles}</Collapse>
+    </div>
   );
 }
