0b77c50a3c596e4a092cc4a8419464cd540ba3a3
Emily Lim <emily.elim04@gmail.com>
2021-03-20 19:54:57 -0400

    remove unused button

--- client/src/components/News/NewsList.jsx
+++ client/src/components/News/NewsList.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Button } from "@material-ui/core";
 
 import NewsListItem from "./NewsListItem";
 import "./NewsList.scss";
