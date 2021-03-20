b5a793a8e45098548e8a9e7f88087342a64340d7
Emily Lim <emily.elim04@gmail.com>
2021-03-20 19:56:07 -0400

    remove spacing

--- client/src/components/UserLanding/WatchTable.jsx
+++ client/src/components/UserLanding/WatchTable.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Tooltip from "@material-ui/core/Tooltip";
-import { IconButton } from "@material-ui/core";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  Tooltip,
+} from "@material-ui/core";
+
 import DeleteIcon from "@material-ui/icons/Delete";
 
 import { useAuth } from "../../contexts/AuthContext";
