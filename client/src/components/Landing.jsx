5f85322b2a301034c1f0876d0ed22f25838a7ce9
Emily Lim <emily.elim04@gmail.com>
2021-03-20 11:46:30 -0400

    add logo

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,31 +5,9 @@ import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 
 export default function Landing() {
-  const [error, setError] = useState("");
-
-  const { currentUser, logout } = useAuth();
-  const history = useHistory();
-
-  async function handleLogout() {
-    setError("");
-    try {
-      await logout();
-      history.push("/login");
-    } catch {
-      setError("Failed to log out");
-    }
-  }
-
   return (
     <>
-      <Card>
-        <h2 className="text-center mb-4">Profile</h2>
-        {error && <Alert severity="error">{error}</Alert>}
-        <strong>Email:</strong> {currentUser && currentUser.user_email}
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button onClick={handleLogout}>Log Out</Button>
-      </div>
+      <img src="./images/stonks.svg" alt="logo" />
     </>
   );
 }
