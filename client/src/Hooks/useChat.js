33fb3e17b866331b812e9e8ffac9790589d034e5
Emily Lim <emily.elim04@gmail.com>
2021-03-20 22:21:50 -0400

    create useChat hook on front end and add socketio client

---
+++ client/src/Hooks/useChat.js
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
+import socketIOClient from "socket.io-client";
+
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const SOCKET_SERVER_URL = "http://localhost:8000";
+
+export default useChat = (roomId) => {
+  const [messages, setMessages] = useState([]);
+  const socketRef = useRef();
+
+  useEffect(() => {
+    //create websocket connection
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+      query: { roomId },
+    });
+
+    socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+      const incomingMessage = {
+        ...message,
+        ownedByCurrentUser: message.senderId === socketRef.current.id,
+      };
+      setMessages((prevMessages) => [...prevMessages, incomingMessage]);
+    });
+
+    //cleanup aka close socket connection when connection is closed
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, [roomId]);
+
+  const sendMessage = (messageBody) => {
+    socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+      body: messageBody,
+      senderId: socketRef.current.id,
+    });
+  };
+
+  return { messages, sendMessage };
+};
