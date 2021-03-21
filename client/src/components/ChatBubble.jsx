8653f71137cf5847523d6df37d232b024d76e382
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-20 22:23:43 -0400

    working on chat component

---
+++ client/src/components/ChatBubble.jsx
@@ -0,0 +1,3 @@
+export default ChatBubble = (props) => {
+  return <ListItem></ListItem>;
+};
---
+++ client/src/components/ChatRoom.jsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+
+export default ChatRoom = (props) => {
+  return (
+    <Card>
+      <CardHeader title="Chat"></CardHeader>
+      <CardContent>
+        <List></List>
+      </CardContent>
+    </Card>
+  );
+};
