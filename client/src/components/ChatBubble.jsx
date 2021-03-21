dc1a28c212ab3f4a8fd687223a90582060f236d5
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-21 11:05:40 -0400

    adding username and timestamp for the users

--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -1,12 +1,23 @@
 import { CardContent, Typography, ListItem, Card } from "@material-ui/core";
 
-const ChatBubble = ({ body, ownedByCurrentUser }) => {
+const ChatBubble = ({ body, username, timestamp, ownedByCurrentUser }) => {
   return (
     <div>
       <ListItem primary="message">
         <Card>
           <CardContent style={{ padding: "6px 10px" }}>
-            <Typography>{body}</Typography>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography style={{ marginRight: 10 }} variant="caption">
+                {username}
+              </Typography>
+              <Typography variant="caption">{timestamp}</Typography>
+            </div>
+            <Typography variant="body2">{body}</Typography>
           </CardContent>
         </Card>
       </ListItem>
--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -9,24 +9,41 @@ import {
 import { useState } from "react";
 import useChat from "../Hooks/useChat";
 import ChatBubble from "./ChatBubble";
+import { useAuth } from "../contexts/AuthContext";
 
 const ChatRoom = ({ company }) => {
   const { messages, sendMessage } = useChat(company);
   const [value, setValue] = useState("");
+  const { currentUser } = useAuth();
 
   const listContent = messages.map((message, index) => {
     return (
       <ChatBubble
         key={index}
         body={message.body}
+        username={message.username}
+        timestamp={message.timestamp}
         ownedByCurrentUser={message.ownedByCurrentUser}
       />
     );
   });
 
   const handleSendMessage = () => {
-    sendMessage(value);
-    setValue("");
+    if (value.length) {
+      currentUser
+        ? sendMessage(value, currentUser.username)
+        : sendMessage(value);
+      setValue("");
+    }
+  };
+
+  const onEnterPressed = (e) => {
+    if (e.key === "Enter" && value.length) {
+      currentUser
+        ? sendMessage(value, currentUser.username)
+        : sendMessage(value);
+      setValue("");
+    }
   };
 
   return (
@@ -45,6 +62,7 @@ const ChatRoom = ({ company }) => {
           value={value}
           onChange={(event) => setValue(event.target.value)}
           style={{ width: "80%", marginBottom: "5px" }}
+          onKeyDown={onEnterPressed}
         />
         <Button onClick={handleSendMessage}>Send</Button>
       </CardContent>
