bcb8f3ab8dd54dfefc6cfd0dbfe8d691c5ab06ab
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-20 23:13:46 -0400

    finish chatbubbule

--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -1,3 +1,18 @@
-export default ChatBubble = (props) => {
-  return <ListItem></ListItem>;
+import { CardContent, Typography, ListItem, Card } from "@material-ui/core";
+
+const ChatBubble = ({ body, ownedByCurrentUser }) => {
+  return (
+    <div>
+      <ListItem primary="message">
+        <Card>
+          <CardContent>
+            <Typography>{ownedByCurrentUser ? "hello" : "goodbye"}</Typography>
+            <Typography>{body}</Typography>
+          </CardContent>
+        </Card>
+      </ListItem>
+    </div>
+  );
 };
+
+export default ChatBubble;
