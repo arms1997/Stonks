dd73107ae1b942c7f8de760872ce87b690577893
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-22 10:41:34 -0400

    finish base look for the about section, need to add how it works

--- client/src/components/Landing/About.jsx
+++ client/src/components/Landing/About.jsx
@@ -29,54 +29,52 @@ export default function About() {
 
   return (
     <main className="learnMore">
-      <section className="learnMore__graph1">
+      <section className="learnMore__section">
         <Card3D photoClassName={photoClassNames[0]}></Card3D>
-        <div className="learnMore__graph1-desc">
-          <Typography variant="h3" className="learnMore__graph1-desc-1">
+        <div className="learnMore__section-desc">
+          <Typography style={{ marginBottom: 15 }} variant="h3">
             Why use Stonks?
           </Typography>
-          <Typography className="learnMore__graph1-desc-2">
-            Stonks is the ultimate resource for newbies to the stock market who
-            want to learn more about trends in prices!......
+          <Typography align="center" className="learnMore__section-p">
+            Stonks was designed for newcomers in the stock trading game. By
+            correlating news articles with direct changes in the stocks data the
+            application is able to inform users of what may or may not have
+            caused the stock to change.
           </Typography>
         </div>
       </section>
-      <section className="learnMore__graph2">
-        <div className="learnMore__graph2-desc">
-          <Typography variant="h3" className="learnMore__graph2-desc-1">
+      <section className="learnMore__section">
+        <div className="learnMore__section-desc">
+          <Typography variant="h3" style={{ marginBottom: 15 }}>
             Relevant news to time periods in the graph!
           </Typography>
-          <Typography className="learnMore__graph2-desc-2">
-            Stonks is the ultimate resource for newbies to the stock market who
-            want to learn more about trends in prices!......
+          <Typography variant="subtitle1">
+            News is currated for the user upon hovering various highlighted
+            areas. These areas signify where the system was able to correlate
+            news to the change in a company's stock.
           </Typography>
         </div>
+
         <Card3D photoClassName={photoClassNames[1]}></Card3D>
       </section>
-      <section className="learnMore__chatbox">
+      <section className="learnMore__section">
         <Card3D photoClassName={photoClassNames[2]}></Card3D>
-        <div className="learnMore__chatbox-desc">
-          <Typography variant="h3" className="learnMore__chatbox-desc-1">
-            Relevant news to time periods in the graph!
-          </Typography>
+        <div className="learnMore__section-desc">
+          <Typography variant="h3">REAL TIME CHAT, HELL YEAH!</Typography>
           <Typography className="learnMore__chatbox-desc-2">
-            Stonks is the ultimate resource for newbies to the stock market who
-            want to learn more about trends in prices!......
+            What is an application nowadays if it doesn't have a real time chat.
+            Users can join chats for specific stocks or join the general chat
+            available on their homepage, to communicate with other{" "}
+            <strong>Stonks</strong> users.
           </Typography>
         </div>
         <div></div>
       </section>
-      <section className="learnMore__ticker">
+      <section
+        className="learnMore__section"
+        style={{ justifyContent: "center" }}
+      >
         <Card3D photoClassName={photoClassNames[3]}></Card3D>
-        <div className="learnMore__ticker-desc">
-          <Typography variant="h3" className="learnMore__ticker-desc-1">
-            Relevant news to time periods in the graph!
-          </Typography>
-          <Typography className="learnMore__ticker-desc-2">
-            Stonks is the ultimate resource for newbies to the stock market who
-            want to learn more about trends in prices!......
-          </Typography>
-        </div>
       </section>
       <Button onClick={scrollTop}>
         <FontAwesomeIcon id="scrollUp" icon={faArrowCircleUp} size="5x" />
