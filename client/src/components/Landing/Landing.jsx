6c4173dee30d76f35f504ed7cc4d4535c64bb5a8
Emily Lim <emily.elim04@gmail.com>
2021-03-22 13:45:40 -0400

    redirect newly signed up user to their landing page

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 import { useHistory } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 import { Button } from "@material-ui/core";
 
@@ -9,7 +10,13 @@ import CustomButton from "../CustomButton";
 import "./Landing.scss";
 
 export default function Landing() {
+  const { currentUser } = useAuth();
   const history = useHistory();
+
+  if (currentUser) {
+    history.push("/mylanding");
+  }
+
   //fade in animation for logo with buttons, this defines the spring and you pass it to the animated.div
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
