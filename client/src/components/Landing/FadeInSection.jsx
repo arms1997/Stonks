57520b9d575aa5c82b02c4980bac436cf127a276
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-22 10:38:15 -0400

    fade in component

---
+++ client/src/components/Landing/FadeInSection.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function FadeInSection(props) {
+  const [isVisible, setVisible] = useState(true);
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+      style={{ width: "100%" }}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
