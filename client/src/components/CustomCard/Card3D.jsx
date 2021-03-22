3ef2b646b97f7dfda02a2adee19f4fad49b1a6e3
Emily Lim <emily.elim04@gmail.com>
2021-03-22 13:27:41 -0400

    add comments to functions

--- client/src/components/CustomCard/Card3D.jsx
+++ client/src/components/CustomCard/Card3D.jsx
@@ -2,6 +2,7 @@ import { useSpring, animated } from "react-spring";
 
 import "./Card3D.scss";
 
+//react spring calculation taken from docs
 const calc = (x, y) => [
   -(y - window.innerHeight / 2) / 20,
   (x - window.innerWidth / 2) / 20,
@@ -11,9 +12,12 @@ const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 export default function Card3D({ photoClassName }) {
+  //pass props for defining spring and set to update
+  //mass is spring mass, tension is spring energetic load and friction is spring resistance
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 700, friction: 100 },
+    //used 'gentle config'
+    config: { mass: 1, tension: 120, friction: 14 },
   }));
 
   return (
