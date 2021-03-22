bb4e6590cf0f44201ae3e722bd75c059d491630f
Emily Lim <emily.elim04@gmail.com>
2021-03-21 21:37:46 -0400

    create new landing folder for component and styles

---
+++ client/src/components/Landing/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import Card3D from "../CustomCard/Card3D";
+import { Typography } from "@material-ui/core";
+
+import "./About.scss";
+
+export default function About() {
+  const photoClassNames = ["graph", "graphWLock", "chatBox", "ticker"];
+
+  return (
+    <main className="learnMore">
+      <section className="learnMore__graph1">
+        <Card3D photoClassName={photoClassNames[0]}></Card3D>
+        <div className="learnMore__graph1-desc">
+          <Typography variant="h3" className="learnMore__graph1-desc-1">
+            Why use Stonks?
+          </Typography>
+          <Typography className="learnMore__graph1-desc-2">
+            Stonks is the ultimate resource for newbies to the stock market who
+            want to learn more about trends in prices!......
+          </Typography>
+        </div>
+      </section>
+      <section className="learnMore__graph2">
+        <div className="learnMore__graph2-desc">
+          <Typography variant="h3" className="learnMore__graph2-desc-1">
+            Relevant news to time periods in the graph!
+          </Typography>
+          <Typography className="learnMore__graph2-desc-2">
+            Stonks is the ultimate resource for newbies to the stock market who
+            want to learn more about trends in prices!......
+          </Typography>
+        </div>
+        <Card3D photoClassName={photoClassNames[1]}></Card3D>
+      </section>
+      <section className="learnMore__chatbox">
+        <Card3D photoClassName={photoClassNames[2]}></Card3D>
+        <div className="learnMore__chatbox-desc">
+          <Typography variant="h3" className="learnMore__chatbox-desc-1">
+            Relevant news to time periods in the graph!
+          </Typography>
+          <Typography className="learnMore__chatbox-desc-2">
+            Stonks is the ultimate resource for newbies to the stock market who
+            want to learn more about trends in prices!......
+          </Typography>
+        </div>
+        <div></div>
+      </section>
+      <section className="learnMore__ticker">
+        <Card3D photoClassName={photoClassNames[3]}></Card3D>
+        <div className="learnMore__ticker-desc">
+          <Typography variant="h3" className="learnMore__ticker-desc-1">
+            Relevant news to time periods in the graph!
+          </Typography>
+          <Typography className="learnMore__ticker-desc-2">
+            Stonks is the ultimate resource for newbies to the stock market who
+            want to learn more about trends in prices!......
+          </Typography>
+        </div>
+        <div></div>
+      </section>
+    </main>
+  );
+}
---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import { useHistory, Link } from "react-router-dom";
+
+import { Button } from "@material-ui/core";
+
+import CustomButton from "../CustomButton";
+
+import Card3D from "../CustomCard/Card3D";
+
+import "./Landing.scss";
+
+export default function Landing() {
+  const history = useHistory();
+  //fade in animation for logo with buttons
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+
+  const handleLoginClick = () => {
+    history.push("/login");
+  };
+
+  const handleSignUpClick = () => {
+    history.push("/signup");
+  };
+
+  const handleLearnMoreClick = () => {
+    history.push("/learnmore");
+  };
+
+  return (
+    <main className="landing">
+      <section className="landing__logoWithButton">
+        <animated.div style={props}>
+          <img src="./images/stonkslightfont.svg" alt="logo" />
+          <div className="landing__logoWithButton-buttons">
+            <CustomButton
+              className="landing__logoWithButton-buttons-login"
+              onClick={handleLoginClick}
+            >
+              Log In
+            </CustomButton>
+            <CustomButton
+              className="landing__logoWithButton-buttons-signup"
+              onClick={handleSignUpClick}
+            >
+              Sign Up
+            </CustomButton>
+          </div>
+          <Button className="landing__link" onClick={handleLearnMoreClick}>
+            Learn More Here
+          </Button>
+        </animated.div>
+      </section>
+    </main>
+  );
+}
